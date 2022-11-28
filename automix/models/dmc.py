36243afcbbf4e8cc6018ab3d02e6a77a35793b73
csteinmetz1 <csteinmetz1@gmail.com>
2022-11-28 18:16:26 +0000

    modified dmc that computes context using only active channels

--- automix/models/dmc.py
+++ automix/models/dmc.py
@@ -380,8 +380,14 @@ class DifferentiableMixingConsole(torch.nn.Module):
         e = e.view(bs, num_tracks, -1)  # (bs, num_tracks, d_embed)
 
         # generate the "context" embedding
-        c = e.mean(dim=1, keepdim=True)  # (bs, 1, d_embed)
-        c = c.repeat(1, num_tracks, 1)  # (bs, num_tracks, d_embed)
+        c = []
+        for bidx in range(bs):
+            c_n = e[bidx, ~track_mask[bidx, :], :].mean(
+                dim=0, keepdim=True
+            )  # (bs, 1, d_embed)
+            c_n = c_n.repeat(num_tracks, 1)  # (bs, num_tracks, d_embed)
+            c.append(c_n)
+        c = torch.stack(c, dim=0)
 
         # fuse the track embs and context embs
         ec = torch.cat((e, c), dim=-1)  # (bs, num_tracks, d_embed*2)
