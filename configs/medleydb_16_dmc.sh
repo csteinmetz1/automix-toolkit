8e8b625ecc4e925655e7e69e4a3ecd22e0207ab7
csteinmetz1 <csteinmetz1@gmail.com>
2022-11-30 09:55:15 +0000

    new training configs

---
+++ configs/medleydb_16_dmc.sh
@@ -0,0 +1,18 @@
+CUDA_VISIBLE_DEVICES=3 python scripts/train.py \
+--dataset_dir \
+"/import/c4dm-datasets/MedleyDB_V1/V1" \
+"/import/c4dm-datasets/MedleyDB_V2/V2" \
+--dataset_name "MedleyDB" \
+--log_dir "/import/c4dm-datasets-ext/automix-toolkit" \
+--automix_model "dmc" \
+--train_length 262144 \
+--val_length 262144 \
+--accelerator gpu \
+--devices 1 \
+--batch_size 16 \
+--lr 1e-4 \
+--max_epochs 400 \
+--max_num_tracks 16 \
+--num_workers 8 \
+--gradient_clip_val 1.0 \
+--schedule "cosine" \
